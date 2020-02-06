c4e10570c5a218c459f27f31c4d08e86da38f3fd
Joe Spinelli <josephspinelli@yahoo.com>
2020-02-06 10:51:46 -0600

    finished creating first custom module route and menu link, created rsvplist module

--- modules/custom/mymodule/src/Controller/FirstController.php
+++ modules/custom/mymodule/src/Controller/FirstController.php
@@ -1,18 +1,21 @@
 <?php
 /**
  * @file
- * Contains \Drupal\mymodule\Controller\MyModuleController.
+ * Contains \Drupal\mymodule\Controller\MyModuleController
  */
 
 namespace Drupal\mymodule\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
+/**
+ *
+ */
 class FirstController extends ControllerBase {
   public function content() {
-    return array(
+    return [
       '#type' => 'markup',
       '#markup' => t('This is my menu linked custom page'),
-    );
+    ];
   }
 }
