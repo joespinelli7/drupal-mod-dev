8e4774c077a9e18ead7c2b86e7b23033253fda3a
Joe Spinelli <josephspinelli@yahoo.com>
2020-02-20 12:57:10 -0600

    created .module and .services for altering form and enabling service

---
+++ modules/custom/rsvplist/src/EnablerService.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\rsvplist\EnablerService
+ */
+
+namespace Drupal\rsvplist;
+
+use Drupal\Core\Database\Database;
+use Drupal\Node\Entity\Node;
+
+/**
+ * Defines a service for managing RSVP list enabledd for nodes
+ */
+
+class EnablerService {
+  /**
+   * Constructor
+   */
+
+  public function __construct() {
+
+  }
+}
