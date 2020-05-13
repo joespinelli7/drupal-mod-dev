7ee1737bb7d2f4078e6ddf6f79f1522dfce9c899
Joe Spinelli <josephspinelli@yahoo.com>
2020-05-13 16:58:46 -0400

    commits

--- modules/custom/redirect_unauthenticated_user/src/Form/RedirectSettingsForm.php
+++ modules/custom/redirect_unauthenticated_user/src/Form/RedirectSettingsForm.php
@@ -31,6 +31,16 @@ class RedirectSettingsForm extends ConfigFormBase {
       'redirect_unauthenticated_user.settings'
     ];
   }
+
+  /**
+   * Simple helper to debug to the console
+   *
+   * @param  object, array, string $data
+   * @return string
+   */
+  function debugToConsole($msg) {
+    echo "<script>console.log(".json_encode($msg).")</script>";
+  }
   /**
    * {@inheritdoc}
    */
@@ -41,18 +51,13 @@ class RedirectSettingsForm extends ConfigFormBase {
     (
       '#prefix' => '<h3>',
       '#suffix' => '</h3>',
-      '#markup' => t('URL redirection (Ex: https://www.hidglobal.com => https://www.hidglobal.com/support)'),
+      '#markup' => t('Handles redirection of unauthenticated users from front page and any 404 error to a specified URL below. Format: https://www.hidglobal.com'),
       '#weight' => -100,
     );
-    $form['url_from'] = ['#type' => 'container', '#attributes' => ['class' => ['container-inline']]];
-    $form['url_from']['redirecting_url'] = [
-      '#title' => $this->t('From: https://baseurl.com/'),
+    $form['url_container'] = ['#type' => 'container', '#attributes' => ['class' => ['container-inline']]];
+    $form['url_container']['destination_url'] = [
       '#type' => 'url',
-    ];
-    $form['url_to'] = ['#type' => 'container', '#attributes' => ['class' => ['container-inline']]];
-    $form['url_to']['destination_url'] = [
-      '#type' => 'url',
-      '#title' => $this->t('To'),
+      '#title' => $this->t('Redirect to'),
     ];
 //    $form['frame'] = ['#type' => 'container', '#attributes' => ['class' => ['container-inline']]];
 //    $form['frame']['e_mail'] = [
@@ -60,8 +65,40 @@ class RedirectSettingsForm extends ConfigFormBase {
 //      '#title' => $this->t('EMail'),
 //    ];
 //    $form['array_filter'] = ['#type' => 'value', '#value' => TRUE];
-
+//    $this->debugToConsole($form['url_container']);
     return parent::buildForm($form, $form_state);
+  }
+  /**
+   * (@inheritdoc)
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    $value = $form_state->getValue('email');
+    if ($value != \Drupal::service('email.validator')->isValid($value)) {
+      $form_state->setErrorByName('email', t('The email address %mail is not valid', ['%mail' => $value]));
+      return;
+    }
+
+    $this->debugToConsole($form_state->getValue('url_container'));
+//    $value = trim($element['#value']);
+//    $form_state
+//      ->setValueForElement($element, $value);
+//    if ($value !== '' && !UrlHelper::isValid($value, TRUE)) {
+//      $form_state
+//        ->setError($element, t('The URL %url is not valid.', array(
+//          '%url' => $value,
+//        )));
+//    }
+//    $node = \Drupal::routeMatch()->getParameter('node');
+//    // Check if email is already set up for this node
+//    $select = Database::getConnection()->select('rsvplist', 'r');
+//    $select->fields('r', ['nid']);
+//    $select->condition('nid', $node->id());
+//    $select->condition('mail', $value);
+//    $results = $select->execute();
+//    if (!empty($results->fetchCol())) {
+//      // We found a row with the specified nid and email
+//      $form_state->setErrorByName('email', t('The address %mail is already subscribed to the list.', ['%mail' => $value]));
+//    }
   }
   /**
    * {@inheritdoc}
